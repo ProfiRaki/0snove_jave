d481128c9fe702e05e622e9ffaea88edbc599dd5
Dragan Radosevic <profiraki.kg@gmail.com>
2022-05-15 16:39:55 +0200

    Domaci 13.05.2022. Zadatak3

---
+++ osnove_jave/src/main/java/d13_05_2022/Zadatak3.java
@@ -0,0 +1,30 @@
+package d13_05_2022;
+//         Napisati program koji ima niz brojeva duzine 10.
+//        Niz je fiksnih vrednosti (ne u nosi ih korisnik)
+//        a zatim se od korisnika ucitava pozicija elementa koji zeli da izbaci iz niza.
+//        Pre i nakon izbacivanja ispisati niz na ekranu.
+//        Ukoliko korisnik unese poziciju van opsega niza, ispisati poruku o gresci.
+//        Commitujte zadatak i pushujte na github sa porukom
+//        Domaci 13.05.2022 Zadatak 3 brisanje iz niza“
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zadatak3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> niz = new ArrayList<Integer>();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            niz.add(r.nextInt(10));
+        }
+        System.out.println("Unesi poziciju elementa za izbacivanje: ");
+        int pozicijaZaIzbacivanje = s.nextInt(10);
+        System.out.println(niz);
+        niz.remove(pozicijaZaIzbacivanje);
+        System.out.println(niz);
+    }
+}
